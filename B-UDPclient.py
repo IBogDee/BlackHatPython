f1e0f44d6c1d31976d378bea8c371bcd9efd2c4c
IBogDee <ibogdee@gmail.com>
2024-07-17 14:06:04 +0300

    FirstUpdate

---
+++ B-UDPclient.py
@@ -0,0 +1,15 @@
+import socket 
+
+target_host = "www.google.com"
+target_port = 80
+
+#create a socket object 
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#send some data
+client.sendto("AAABBBCCC".encode(),(target_host,target_port))
+
+#rcevie some data
+data, addr = client.recvfrom(4096)
+
+print(data)
